a29704efce39c1e96e4da1cd190714aafb981671
yuumi <gin728emma218@gmail.com>
2023-12-11 18:51:41 +0900

    編集を確定し、一覧ページに遷移

--- src/app/todos/[id]/edit/page.tsx
+++ src/app/todos/[id]/edit/page.tsx
@@ -1,78 +1,93 @@
 "use client";
 
 import React, { useState } from "react";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { useParams } from "next/navigation";
-import { useRecoilValue } from "recoil";
+import { useRouter, useParams } from "next/navigation";
+import { useRecoilState } from "recoil";
 import { todosState } from "../../../components/atoms";
-import Link from 'next/link';
+import { Todo } from "../../../../types/Todo";
 import { Button } from "@mui/material";
 
 const page = () => {
-
+  const router = useRouter();
   const params = useParams();
-  const todos = useRecoilValue(todosState);
+  const [todos, setTodos] = useRecoilState(todosState);
   console.log(params);
 
-  const todo = todos.find((todo) => todo.id === params.id);
+  const todo: Todo | undefined = todos.find((todo) => todo.id === params.id);
+  const [title, setTitle] = useState<any>(todo?.title);
+  const [responsible, setResponsible] = useState<any>(todo?.responsible);
+  const [status, setStatus] = useState<any>(todo?.status);
+  const [detail, setDetail] = useState<any>(todo?.detail);
+  const [deadline, setDeadline] = useState<any>(todo?.deadline);
 
-  const [title, setTitle] = useState('');
-  const [responsible, setResponsible] = useState('');
-  const [status, setStatus] = useState('');
-  const [detail, setDetail] = useState('');
-  const [deadline, setDeadline] = useState('');
+  const handleEditSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === params.id) {
+        return {
+          ...todo,
+          title: title,
+          responsible: responsible,
+          status: status,
+          detail: detail,
+          deadline: deadline,
+        };
+      }
+      return todo;
+    });
+  
+    setTodos(updatedTodos);
+    router.push('/todos');
+  }
 
   return (
     <div>
       {todo ? (
-        <form>
+        <form onSubmit={handleEditSubmit}>
           <label htmlFor="title">タスク名</label>
           <input
             id="title"
             type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            />
+          />
           <label htmlFor="responsible">担当者</label>
           <input
             id="responsible"
             type="text"
-            value={todo.responsible}
+            value={responsible}
             onChange={(e) => setResponsible(e.target.value)}
             />
           <label htmlFor="status">ステート</label>
           <input
             id="status"
             type="text"
-            value={todo.status}
+            value={status}
             onChange={(e) => setStatus(e.target.value)}
-            />
+          />
           <label htmlFor="detail">内容</label>
           <input
             id="detail"
             type="text"
-            value={todo.detail}
+            value={detail}
             onChange={(e) => setDetail(e.target.value)}
-            />
+          />
           <label htmlFor="deadline">期限</label>
           <input
             id="deadline"
             type="text"
-            value={todo.deadline}
+            value={deadline}
             onChange={(e) => setDeadline(e.target.value)}
-            />
+          />
+
+          <Button type="submit">確定</Button>
         </form>
       ) : (
         <p>Todo is not found</p>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
