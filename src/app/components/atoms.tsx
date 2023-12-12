c592aea35a7f329d6fa1a374914e6a7c50e538ed
yuumi <gin728emma218@gmail.com>
2023-12-12 21:13:33 +0900

    recoil-persistを削除

--- src/app/components/atoms.tsx
+++ src/app/components/atoms.tsx
@@ -2,13 +2,9 @@
 
 import { Todo } from '../../types/Todo'
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
-
-const { persistAtom } = recoilPersist();
 
 export const todosState = atom<Todo[]>({
   key: "todos",
   default: [
   ],
-  effects_UNSTABLE: [persistAtom]
 });
--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -62,31 +62,31 @@ const Page = () => {
         <h2 className='text-4xl font-bold'>Todo一覧</h2>
 
         <TableContainer component={Paper} className='mt-12'>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>タスク名</TableCell>
-              <TableCell align="right">担当者</TableCell>
-              <TableCell align="right">ステータス</TableCell>
-              <TableCell align="right">期限</TableCell>
-              <TableCell align="right">詳細</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {todos.map((todo) => (
-              <TableRow
-                key={todo.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="todo">{todo.title}</TableCell>
-                <TableCell align="right">{todo.responsible}</TableCell>
-                <TableCell align="right">{todo.status}</TableCell>
-                <TableCell align="right">{todo.deadline}</TableCell>
-                <TableCell align="right"><Link href={`/todos/${todo.id}`}>詳細</Link></TableCell>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>タスク名</TableCell>
+                <TableCell align="right">担当者</TableCell>
+                <TableCell align="right">ステータス</TableCell>
+                <TableCell align="right">期限</TableCell>
+                <TableCell align="right">詳細</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {todos.map((todo) => (
+                <TableRow
+                  key={todo.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell>{todo.title}</TableCell>
+                  <TableCell align="right">{todo.responsible}</TableCell>
+                  <TableCell align="right">{todo.status}</TableCell>
+                  <TableCell align="right">{todo.deadline}</TableCell>
+                  <TableCell align="right"><Link href={`/todos/${todo.id}`}>詳細</Link></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </TableContainer>
 
         <Link href={"/todos/create"}>
