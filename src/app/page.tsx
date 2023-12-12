d85f709a71fdf52419baf77a95e6bcd97a884407
yuumi <gin728emma218@gmail.com>
2023-12-12 21:33:20 +0900

    /に新規登録とログインボタンを設置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,27 @@
 /**
- * ログインページ（/）
+ * トップページ（/）
  */
 
 "use client";
-
-import React, { useState } from 'react';
-import { app } from "./firebase"
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Link from "next/link";
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { useRouter } from "next/navigation"
+import { useAuthContext } from './context/AuthContext';
 
 export default function Home() {
 
   const router = useRouter();
-  const auth = getAuth(app);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { user } = useAuthContext();
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await signInWithEmailAndPassword(auth, email, password);
+  if (!user) {
+    return (
+      <div className="flex items-center justify-center gap-4">
+        <Button><Link href="/signup">新規登録</Link></Button>
+        <Button><Link href="/signin">ログイン</Link></Button>
+      </div>
+    );
+  } else {
+    //userがログインしている時は、todosページにリダイレクト
     router.push('/todos');
   }
-  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.currentTarget.value)
-  }
-  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.currentTarget.value)
-  }
-
-  return (
-    <div className="flex flex-col items-center">
-
-      <h2 className="text-4xl font-bold">Next Todo App</h2>
-
-      <form
-        onSubmit={handleSubmit}
-        className='mt-6'
-      >
-        <div className='flex flex-col items-start'>
-          <TextField
-            id="standard-email-input"
-            label="email"
-            type="email"
-            autoComplete="current-email"
-            variant="standard"
-            onChange={handleChangeEmail}
-          />
-          <TextField
-            id="standard-password-input"
-            label="password"
-            type="password"
-            autoComplete="current-password"
-            variant="standard"
-            onChange={handleChangePassword}
-          />
-        </div>
-
-        <div className="flex justify-center mt-4">
-          <Button type="submit" variant="contained">
-            ログイン
-          </Button>
-        </div>
-      </form>
-
-      <div className="flex justify-end mt-6">
-          <Link href={"/signup"}>
-            新規登録はこちら
-          </Link>
-        </div>
-    </div>
-  )
 }
---
+++ src/app/signin/page.tsx
@@ -0,0 +1,79 @@
+/**
+ * ログインページ（/signin）
+ */
+
+"use client";
+
+import React, { useState } from 'react';
+import { app } from "../firebase"
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Link from "next/link";
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useRouter } from "next/navigation"
+
+const Page =() => {
+
+  const router = useRouter();
+  const auth = getAuth(app);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await signInWithEmailAndPassword(auth, email, password);
+    router.push('/todos');
+  }
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value)
+  }
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value)
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+
+      <h2 className="text-4xl font-bold">ログイン</h2>
+
+      <form
+        onSubmit={handleSubmit}
+        className='mt-6'
+      >
+        <div className='flex flex-col items-start'>
+          <TextField
+            id="standard-email-input"
+            label="email"
+            type="email"
+            autoComplete="current-email"
+            variant="standard"
+            onChange={handleChangeEmail}
+          />
+          <TextField
+            id="standard-password-input"
+            label="password"
+            type="password"
+            autoComplete="current-password"
+            variant="standard"
+            onChange={handleChangePassword}
+          />
+        </div>
+
+        <div className="flex justify-center mt-4">
+          <Button type="submit" variant="contained">
+            ログイン
+          </Button>
+        </div>
+      </form>
+
+      <div className="flex justify-end mt-6">
+          <Link href={"/signup"}>
+            新規登録はこちら
+          </Link>
+        </div>
+    </div>
+  )
+}
+
+
+export default Page;
