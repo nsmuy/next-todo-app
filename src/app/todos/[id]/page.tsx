9718bfab6e1f96df7313c7827a2d9749323cd655
yuumi <gin728emma218@gmail.com>
2023-12-12 23:34:51 +0900

    一覧をコンポーネント化

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -6,21 +6,13 @@
 
 import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import { useParams } from "next/navigation";
 import { useRecoilState } from "recoil";
 import { todosState } from "../../components/atoms";
-import Link from 'next/link';
-import { Button } from "@mui/material";
 import { Todo } from "../../../types/Todo";
 import { db } from "@/app/firebase";
 import { collection, query, where, onSnapshot, Timestamp, addDoc, orderBy } from "firebase/firestore"; 
+import TodoList from "../components/TodoList";
 
 type Comment = {
   docId: string,
@@ -38,7 +30,7 @@ const Page = () => {
 
   useEffect(() => {
     const commentsRef = collection(db, 'comments');
-    
+
     // idフィールドの値とURLのパラメーターIDが一致し、sendAtでソートされたドキュメントを取得
     const q = query(commentsRef, where('id', '==', params.id), orderBy('sendAt', 'asc'));
   
@@ -69,54 +61,12 @@ const Page = () => {
     setSendComments('');
   }
 
-  const handleTodoDelete = () => {
-    const newTodos = todos.filter(todo => todo.id !== params.id);
-    setTodos(newTodos);
-  }
-
   return (
     <div className='flex flex-col items-center'>
       <h2 className="text-4xl font-bold">タスク詳細</h2>
       {todo ? (
         <div className="flex flex-col items-center mt-6">
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>タスク名</TableCell>
-                  <TableCell align="right">担当者</TableCell>
-                  <TableCell align="right">ステータス</TableCell>
-                  <TableCell align="right">内容</TableCell>
-                  <TableCell align="right">期限</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow
-                  key={todo.id}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="todo">
-                    {todo.title}
-                  </TableCell>
-                  <TableCell align="right">{todo.responsible}</TableCell>
-                  <TableCell align="right">{todo.status}</TableCell>
-                  <TableCell align="right">{todo.detail}</TableCell>
-                  <TableCell align="right">{todo.deadline}</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-
-          <div className="w-full flex justify-center mt-6">
-            <Link href='/todos'>
-              <Button>前のページに戻る</Button>
-            </Link>
-            <Link href={`/todos/${todo.id}/edit`}>
-              <Button>編集</Button>
-            </Link>
-            <Button onClick={handleTodoDelete}>削除</Button>
-          </div>
-
+          <TodoList />
           {/* コメント */}
           <div className="w-full mt-6">
             <h3 className="text-2xl font-bold">コメント</h3>
---
+++ src/app/todos/components/TodoList.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Link from 'next/link';
+import { Button } from "@mui/material";
+import { useRouter, useParams } from "next/navigation";
+import { useRecoilState } from "recoil";
+import { todosState } from "@/app/components/atoms";
+
+const TodoList = () => {
+
+  const router = useRouter();
+  const params = useParams();
+  const [todos, setTodos] = useRecoilState(todosState);
+  const todo = todos.find((todo) => todo.id === params.id);
+  if (!todo) {
+    return null;
+  }
+
+  const handleTodoDelete = () => {
+    const newTodos = todos.filter(todo => todo.id !== params.id);
+    setTodos(newTodos);
+    router.push('/todos');
+  }
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>タスク名</TableCell>
+              <TableCell align="right">担当者</TableCell>
+              <TableCell align="right">ステータス</TableCell>
+              <TableCell align="right">内容</TableCell>
+              <TableCell align="right">期限</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow key={todo.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+              <TableCell component="th" scope="todo">
+                {todo.title}
+              </TableCell>
+              <TableCell align="right">{todo.responsible}</TableCell>
+              <TableCell align="right">{todo.status}</TableCell>
+              <TableCell align="right">{todo.detail}</TableCell>
+              <TableCell align="right">{todo.deadline}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <div className="w-full flex justify-center mt-6">
+        <Link href="/todos">
+          <Button>前のページに戻る</Button>
+        </Link>
+        <Link href={`/todos/${todo.id}/edit`}>
+          <Button>編集</Button>
+        </Link>
+        <Button onClick={handleTodoDelete}>削除</Button>
+      </div>
+    </>
+  );
+};
+
+export default TodoList;
