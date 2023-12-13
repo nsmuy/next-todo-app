0e17e1bf79c76a2ddfa48663e873fdec6334af0b
yuumi <gin728emma218@gmail.com>
2023-12-14 00:28:46 +0900

    git動かない

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAeZPQoFjgwX3jBrSb_UnIMB3_DBqOxFmE",
-  authDomain: "next-todo-app-33551.firebaseapp.com",
-  projectId: "next-todo-app-33551",
-  storageBucket: "next-todo-app-33551.appspot.com",
-  messagingSenderId: "56843187015",
-  appId: "1:56843187015:web:7db78ec1b8ac8226ca1262"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
